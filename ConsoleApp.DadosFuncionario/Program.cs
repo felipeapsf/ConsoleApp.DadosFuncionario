f24dfb2b2fceb15d7a4e3d3c5a4a76705157967f
Felipe Fidalgo <felipe_apsf@hotmail.com>
2021-01-26 11:39:36 -0300

    Add project files.

---
+++ ConsoleApp.DadosFuncionario/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using ConsoleApp.DadosFuncionario.Entities;
+
+namespace ConsoleApp.DadosFuncionario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter full file path: ");
+            string path = Console.ReadLine();
+            Console.Write("Enter salary: ");
+            double limit = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            List<Employee> list = new List<Employee>();
+
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string[] fields = sr.ReadLine().Split(",");
+                        string name = fields[0];
+                        string email = fields[1];
+                        double salary = double.Parse(fields[2], CultureInfo.InvariantCulture);
+                        list.Add(new Employee(name, email, salary));
+                    }
+                }
+                Console.WriteLine($"Email of people whose salary is more than {limit.ToString("F2", CultureInfo.InvariantCulture)}:");
+                var emails = list.Where(e => e.Salary > limit).OrderBy(e => e.Email).Select(e => e.Email);
+                foreach (var m in emails)
+                {
+                    Console.WriteLine(m);
+                }
+                var sum = list.Where(s => s.Name[0] == 'M').Sum(s => s.Salary);
+                Console.WriteLine("Sum of salary of people whose name starts with 'M': " + sum.ToString("F2", CultureInfo.InvariantCulture));
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
